06e806c96bc3efd8ed1cdeda4e32f8297429da4d
Arnas.Kazlauskas <arnas.kazlauskas@kilo.health>
2023-04-05 19:29:24 +0300

    feat(2-add-two-numbers): solution

---
+++ 2-add-two-numbers/2-add-two-numbers.go
@@ -0,0 +1,38 @@
+package leetcode
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func AddTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	s := ListNode{}
+	t := &s
+	carry := 0
+	for {
+		if l1 == nil {
+			l1 = &ListNode{}
+		}
+		if l2 == nil {
+			l2 = &ListNode{}
+		}
+		t.Val += l1.Val + l2.Val + carry
+		carry = 0
+		if t.Val >= 10 {
+			t.Val -= 10
+			carry = 1
+		}
+
+		if l1.Next == nil && l2.Next == nil {
+			if carry != 0 {
+				t.Next = &ListNode{Val: 1}
+			}
+			break
+		}
+		l1 = l1.Next
+		l2 = l2.Next
+		t.Next = &ListNode{}
+		t = t.Next
+	}
+	return &s
+}
