fe669b5b08f8436980bdd83e74323975bf1073f6
prodigeris <arnas.kazlauskas@kilo.health>
2023-06-14 22:21:20 +0100

    feat(two-sum): pointer solution

--- 167-two-sum/two-sum.go
+++ 167-two-sum/two-sum.go
@@ -1,5 +1,15 @@
 package _67_two_sum
 
-func twoSum(numbers []int, target int) []int {
-	return []int{}
+func twoSum(nums []int, target int) []int {
+	i, j := 0, len(nums)-1
+
+	for nums[i]+nums[j] != target {
+		if nums[i]+nums[j] > target {
+			j--
+		}
+		if nums[i]+nums[j] < target {
+			i++
+		}
+	}
+	return []int{i + 1, j + 1}
 }
