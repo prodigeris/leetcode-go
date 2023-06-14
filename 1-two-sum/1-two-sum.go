38e6ca97c3c367004f8c58e945d8b5722ec441c5
prodigeris <arnas.kazlauskas@kilo.health>
2023-06-14 17:16:00 +0100

    feat(two-sum): sol

--- 1-two-sum/1-two-sum.go
+++ 1-two-sum/1-two-sum.go
@@ -1,5 +1,15 @@
 package __two_sum
 
 func twoSum(nums []int, target int) []int {
+	for i, v := range nums {
+		for j, v2 := range nums {
+			if i == j {
+				continue
+			}
+			if v+v2 == target {
+				return []int{i, j}
+			}
+		}
+	}
 	return []int{}
 }
