bbdfb6d5964c7423ce96154aa85d08367d3e45b1
prodigeris <arnas.kazlauskas@kilo.health>
2023-06-14 21:35:13 +0100

    feat(valid-palindrome): sol

--- 125-valid-palindrome/palindrome.go
+++ 125-valid-palindrome/palindrome.go
@@ -1,5 +1,18 @@
 package _25_valid_palindrome
 
+import (
+	"regexp"
+	"strings"
+)
+
 func isPalindrome(s string) bool {
+	s = strings.ToLower(s)
+	r := regexp.MustCompile("[^a-z0-9]")
+	s = r.ReplaceAllString(s, "")
+	for i := 0; i < len(s)/2; i++ {
+		if s[i] != s[len(s)-i-1] {
+			return false
+		}
+	}
 	return true
 }
--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module leetcode
 
 go 1.15
-
-require golang.org/x/exp v0.0.0-20230522175609-2e198f4a06a1
