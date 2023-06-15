8839c51d5d8f875d2bf071702e451c5f90c928a4
prodigeris <arnas.kazlauskas@kilo.health>
2023-06-15 19:53:48 +0100

    feat(container): sol

--- 11-container-most-water/container.go
+++ 11-container-most-water/container.go
@@ -1,5 +1,21 @@
 package _1_container_most_water
 
 func maxArea(height []int) int {
-	return 0
+	i, j := 0, len(height)-1
+	max := 0
+	for i < j {
+		v1, v2 := height[i], height[j]
+		s := j - i
+		if v1 < v2 {
+			s *= v1
+			i++
+		} else {
+			s *= v2
+			j--
+		}
+		if s > max {
+			max = s
+		}
+	}
+	return max
 }
