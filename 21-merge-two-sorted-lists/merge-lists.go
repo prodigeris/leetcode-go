2f8c7520ff2c8310f8ee3d8d35d837c2ebc4067f
prodigeris <arnas.kazlauskas@kilo.health>
2023-06-16 17:26:11 +0100

    feat(merge-two-linked-lists): new sol

--- 21-merge-two-sorted-lists/merge-lists.go
+++ 21-merge-two-sorted-lists/merge-lists.go
@@ -1,48 +1,22 @@
 package _1_merge_two_sorted_lists
 
 func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
-	var left, right int
-	var result, last *ListNode
-	for true {
-		if list1 == nil {
-			if last == nil {
-				last = list2
-				result = last
-			} else {
-				last.Next = list2
-			}
-			break
-		}
-
-		if list2 == nil {
-			if last == nil {
-				last = list1
-				result = last
-			} else {
-				last.Next = list1
-			}
-			break
-		}
-		left = list1.Val
-		right = list2.Val
-		if left < right {
-			if last != nil {
-				last.Next = list1
-			}
-			last = list1
-			list1 = list1.Next
+	var curr, prev, start *ListNode
+	for list1 != nil || list2 != nil {
+		if list2 == nil || (list1 != nil && list1.Val <= list2.Val) {
+			curr, list1 = list1, list1.Next
 		} else {
-			if last != nil {
-				last.Next = list2
-			}
-			last = list2
-			list2 = list2.Next
+			curr, list2 = list2, list2.Next
 		}
-		if result == nil {
-			result = last
+		if prev != nil {
+			prev.Next = curr
 		}
+		if start == nil {
+			start = curr
+		}
+		prev = curr
 	}
-	return result
+	return start
 }
 
 type ListNode struct {
