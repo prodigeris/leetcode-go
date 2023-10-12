a9009a07e0579b278bbcf0c493ef5ea8abd60df9
prodigeris <arnas.kazlauskas@kilo.health>
2023-10-12 21:26:41 +0300

    feat(141-linked-list-cycle): tortoise solution

--- 141-linked-list-cycle/cycle.go
+++ 141-linked-list-cycle/cycle.go
@@ -17,3 +17,20 @@ func hasCycle(head *ListNode) bool {
 	}
 	return false
 }
+
+func hasCycleTortoise(head *ListNode) bool {
+	if head == nil {
+		return false
+	}
+	slow, fast := head, head
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+
+		if slow == fast {
+			return true
+		}
+	}
+
+	return false
+}
--- 141-linked-list-cycle/cycle_test.go
+++ 141-linked-list-cycle/cycle_test.go
@@ -24,3 +24,24 @@ func TestHasCycle(t *testing.T) {
 		}
 	}
 }
+
+func TestHasCycleTortoise(t *testing.T) {
+
+	pos := &ListNode{Val: 2}
+	head1 := &ListNode{Val: 3, Next: pos}
+	pos.Next = &ListNode{Val: 0, Next: &ListNode{Val: 4, Next: pos}}
+
+	tests := []struct {
+		head *ListNode
+		want bool
+	}{
+		{head1, true},
+	}
+
+	for _, tt := range tests {
+		got := hasCycleTortoise(tt.head)
+		if got != tt.want {
+			t.Errorf("HasCycle = %v; want %v", got, tt.want)
+		}
+	}
+}
