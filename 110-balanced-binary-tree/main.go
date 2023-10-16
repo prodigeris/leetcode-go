d29481bfaf8c798b0c8bef40d918b4c52aa0020a
prodigeris <arnas.kazlauskas@kilo.health>
2023-10-16 14:19:14 +0300

    fix(110-balanced-binary-tree): return early

--- 110-balanced-binary-tree/main.go
+++ 110-balanced-binary-tree/main.go
@@ -18,7 +18,13 @@ func dfs(root *TreeNode) int {
 	}
 
 	l := dfs(root.Left)
+	if l == -1 {
+		return -1
+	}
 	r := dfs(root.Right)
+	if r == -1 {
+		return -1
+	}
 
 	if math.Abs(float64(l-r)) < 2 {
 		return -1
