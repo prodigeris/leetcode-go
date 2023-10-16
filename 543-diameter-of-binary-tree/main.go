2f687fb4d32c16a8e28884857780931aa825fba8
prodigeris <arnas.kazlauskas@kilo.health>
2023-10-16 12:36:08 +0300

    fix(543-diameter): solution

--- 543-diameter-of-binary-tree/main.go
+++ 543-diameter-of-binary-tree/main.go
@@ -7,7 +7,28 @@ type TreeNode struct {
 }
 
 func diameterOfBinaryTree(root *TreeNode) int {
-	return 0
+	m := 0
+	dfs(root, &m)
+	return m
+}
+
+func dfs(root *TreeNode, m *int) int {
+	if root == nil {
+		return 0
+	}
+
+	l := dfs(root.Left, m)
+	r := dfs(root.Right, m)
+	*m = max(*m, l+r)
+
+	return max(l, r) + 1
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
 
 func main() {
