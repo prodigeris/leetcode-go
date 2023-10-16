1f17d1e19621f86b49dbe72ee405417b877df6b1
prodigeris <arnas.kazlauskas@kilo.health>
2023-10-16 22:25:53 +0300

    feat(78-subsets): solution

--- 78-subsets/main.go
+++ 78-subsets/main.go
@@ -1,6 +1,27 @@
 package main
 
+import "fmt"
+
 func subsets(nums []int) [][]int {
-	sets := [][]int{[]int{}}
-	return sets
+	var r [][]int
+
+	dfs(nums, &r, []int{}, 0)
+
+	return r
+}
+
+func dfs(nums []int, r *[][]int, c []int, index int) {
+	if index == len(nums) {
+		copied := make([]int, len(c))
+		copy(copied, c)
+		*r = append(*r, copied)
+		return
+	}
+
+	dfs(nums, r, c, index+1)
+	dfs(nums, r, append(c, nums[index]), index+1)
+}
+
+func main() {
+	fmt.Println(subsets([]int{1, 2, 3}))
 }
--- 78-subsets/main_test.go
+++ 78-subsets/main_test.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"reflect"
+	sort "sort"
 	"testing"
 )
 
@@ -21,16 +23,49 @@ func TestSubsets(t *testing.T) {
 			input:  []int{0},
 			output: [][]int{{}, {0}},
 		},
+
+		{
+			name:  "Example 3",
+			input: []int{9, 0, 3, 5, 7},
+			output: [][]int{
+				{}, {9}, {0}, {0, 9}, {3}, {3, 9}, {0, 3}, {0, 3, 9}, {5},
+				{5, 9}, {0, 5}, {0, 5, 9}, {3, 5}, {3, 5, 9}, {0, 3, 5},
+				{0, 3, 5, 9}, {7}, {7, 9}, {0, 7}, {0, 7, 9}, {3, 7}, {3, 7, 9},
+				{0, 3, 7}, {0, 3, 7, 9}, {5, 7}, {5, 7, 9}, {0, 5, 7},
+				{0, 5, 7, 9}, {3, 5, 7}, {3, 5, 7, 9}, {0, 3, 5, 7}, {0, 3, 5, 7, 9},
+			},
+		},
 	}
 
 	for _, tt := range tests {
-		tt := tt
+		tt := tt // capture range variable
 		t.Run(tt.name, func(t *testing.T) {
 			t.Parallel()
 			got := subsets(tt.input)
-			if !reflect.DeepEqual(got, tt.output) {
+			if !equivalentSets(got, tt.output) {
 				t.Errorf("subsets(%v) = %v; want %v", tt.input, got, tt.output)
 			}
 		})
 	}
+}
+
+// Helper function to determine if two slices of slices have the same sets.
+func equivalentSets(a, b [][]int) bool {
+	mapA, mapB := make(map[string]struct{}), make(map[string]struct{})
+
+	for _, v := range a {
+		sort.Ints(v)
+		mapA[toString(v)] = struct{}{}
+	}
+	for _, v := range b {
+		sort.Ints(v)
+		mapB[toString(v)] = struct{}{}
+	}
+
+	return reflect.DeepEqual(mapA, mapB)
+}
+
+// Helper function to convert a slice of ints to a string representation.
+func toString(a []int) string {
+	return fmt.Sprint(a)
 }
