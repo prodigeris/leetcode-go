d712ac105df126a3d7bfdd538653843005625407
prodigeris <arnas.kazlauskas@kilo.health>
2023-10-16 16:22:56 +0300

    feat(5720-subtree-another-tree): solution

--- 572-subtree-of-another-tree/main.go
+++ 572-subtree-of-another-tree/main.go
@@ -16,11 +16,25 @@ func isSubtree(root *TreeNode, subRoot *TreeNode) bool {
 	}
 
 	if root.Val == subRoot.Val {
-		result := isSubtree(root.Left, subRoot.Left) && isSubtree(root.Right, subRoot.Right)
+		result := isSame(root.Left, subRoot.Left) && isSame(root.Right, subRoot.Right)
 		if result {
 			return true
 		}
 	}
 
 	return isSubtree(root.Left, subRoot) || isSubtree(root.Right, subRoot)
+}
+
+func isSame(root *TreeNode, subRoot *TreeNode) bool {
+	if root == nil && subRoot == nil {
+		return true
+	}
+	if root == nil || subRoot == nil {
+		return false
+	}
+	if root.Val != subRoot.Val {
+		return false
+	}
+
+	return isSame(root.Left, subRoot.Left) && isSame(root.Right, subRoot.Right)
 }
