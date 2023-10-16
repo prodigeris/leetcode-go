1ec4e573c9c88419d2838cfe13a34ed51093e0f3
prodigeris <arnas.kazlauskas@kilo.health>
2023-10-16 15:18:04 +0300

    feat(100-same-tree): solution

--- 100-same-tree/main.go
+++ 100-same-tree/main.go
@@ -7,5 +7,12 @@ type TreeNode struct {
 }
 
 func isSameTree(p *TreeNode, q *TreeNode) bool {
-	return false
+	if p == nil && q == nil {
+		return true
+	}
+	if p == nil || q == nil || p.Val != q.Val {
+		return false
+	}
+
+	return isSameTree(p.Left, q.Left) && isSameTree(p.Right, q.Right)
 }
